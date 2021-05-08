b66aaa0fbd5903301e4556677da7a8ea1a80e39e
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-05-08 18:07:12 -0400

    Update Routes.ts

--- src/Routes.ts
+++ src/Routes.ts
@@ -38,7 +38,7 @@ router.get('/endpoints', (req, res) => {
 	]);
 });
 
-router.get('/test/:test/:tesst', (req, res) => {
+router.get('/test/:test', (req, res) => {
 	console.log(req.query);
 	res.json({ success: true, link: 'https://youtu.be/dQw4w9WgXcQ' });
 });
